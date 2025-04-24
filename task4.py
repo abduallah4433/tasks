438c99d087cf5dc4dfaf157ca3e0db13052a02b6
abduallah4433 <works4433@gmail.com>
2025-04-24 05:33:16 +0300

    second commit

--- task4.py
+++ task4.py
@@ -1,5 +1,5 @@
 test = [5, 4, 17, 19, 30, 2, 7, 10, 45]
 
-filter = lambda func : [i for i in func if i % 2 == 0 ]
+get_odd = lambda func : [i for i in func if i % 2 != 0 ]
 
-print(filter(test))
+print(get_odd(test))
